3286ca6e2e08f59422e58cbf7d3f84f5f372b7e7
TakenakaEri <takenakaeri0423@gmail.com>
2025-07-28 20:40:58 +0900

    テンプレート文字列へ修正

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -28,7 +28,9 @@ calendarLine += "   ".repeat(firstDayOfWeek);
 
 for (let day = 1; day <= dayMonth; day++) {
   const padded = String(day).padStart(2, " ");
-  calendarLine += padded + " ";
+  // 文字列結合ではなくテンプレート文字列
+  // calendarLine += padded + " ";
+  calendarLine += `${padded} `;
   const currentWeekday = (firstDayOfWeek + day - 1) % 7;
   if (currentWeekday === 6) {
     console.log(calendarLine.trimEnd() + "  ");
