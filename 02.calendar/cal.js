b4b4c9cd2acd28a3545e09294a55a0d0cced5972
TakenakaEri <takenakaeri0423@gmail.com>
2025-07-28 18:01:28 +0900

    ||演算子を??に修正

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -10,8 +10,8 @@ const args = minimist(process.argv.slice(2));
 const now = dayjs();
 
 const currentDate = now;
-const year = args.y || currentDate.year();
-const month = args.m || currentDate.month() + 1;
+const year = args.y ?? currentDate.year();
+const month = args.m ?? currentDate.month() + 1;
 
 const targetDate = dayjs(`${year}-${month}-01`);
 const dayMonth = targetDate.daysInMonth();
